94fa7a2306b834c1453a686ace85762b53666c8f
David Enrique Zayas Piña <zyztek@gmail.com>
2025-08-03 18:03:33 +0000

    Auto-commit: 1 added file

---
+++ src/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""
+Initialization file for the TTBT5 Application.
+This file initializes the src package.
+"""
+
+# This file is intentionally left blank.
+# It serves to mark the directory as a Python package.
